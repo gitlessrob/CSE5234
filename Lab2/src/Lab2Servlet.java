e42ed1370734308d744d1baa278b61530972de42
Rob <Rob@172.27.45.217>
2018-09-12 18:15:36 -0400

    Another commit to test

--- Lab2/src/Lab2Servlet.java
+++ Lab2/src/Lab2Servlet.java
@@ -27,7 +27,7 @@ public class Lab2Servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().println("Hello World!");
+		response.getWriter().println("Hello World!!!!!!!!!!!");
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
