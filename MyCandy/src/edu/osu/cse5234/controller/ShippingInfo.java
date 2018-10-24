5db0f2381e4473ab13e9ce5fde74c0504b9fe456
sandhuat <43222349+sandhuat@users.noreply.github.com>
2018-10-24 00:52:32 -0400

    Update ShippingInfo.java

--- MyCandy/src/edu/osu/cse5234/controller/ShippingInfo.java
+++ MyCandy/src/edu/osu/cse5234/controller/ShippingInfo.java
@@ -1,14 +1,55 @@
 package edu.osu.cse5234.controller;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="SHIPPING_INFO")
 public class ShippingInfo {
-	
+	@Transient
 	private String name;
+	@Column(name="ADDRESS1")
 	private String addressLine1;
+	@Column(name="ADDRESS2")
 	private String addressLine2;
+	@Column(name="CITY")
 	private String city;
+	@Column(name="COUNTRY")
+	private String country;
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	@Column(name="EMAIL")
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Column(name="STATE")
 	private String state;
+	@Column(name="POSTAL_CODE")
 	private String zip;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	int id;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
